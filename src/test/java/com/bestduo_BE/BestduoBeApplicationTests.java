be637626fd097cc69a3d8090b09a48bacde59028
NamMinwu <minwoo000625@naver.com>
2025-12-16 17:34:14 +0900

    FEAT : 상세 페이지 유스케이스 기능 추가적으로 못 올린거

---
+++ src/test/java/com/bestduo_BE/BestduoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestduo_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestduoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
